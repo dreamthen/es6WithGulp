85f7d01b7f1ba59f5accad812c835631f27aff00
尹文楷 <yinwk@servyou.com.cn>
2017-10-31 18:10:58 +0800

    add lesson12

--- app/js/class/lesson12.js
+++ app/js/class/lesson12.js
@@ -29,5 +29,80 @@
 }
 
 {
-    let weakSet = new WeakMap();
+    //WeakSet
+    //1. no size property
+    //2. no clear function
+    //3. no 遍历
+    let weakSet = new WeakSet();
+    let weakSetDemo = {name: "yinwk"};
+    weakSet.add(weakSetDemo);
+    console.log(weakSet, weakSet.size);
+    // weakSet.clear();
+    // for (let weakSetItem of weakSet) {
+    //     console.log(weakSetItem);
+    // }
+    console.log(weakSet.has(weakSetDemo));
+    weakSet.delete(weakSetDemo);
+    console.log(weakSet);
+}
+
+{
+    //Map
+    let map = new Map();
+    let o = {name: 'yinwk'};
+    map.set(o, {userName: 'clown laugh at you~'});
+    map.set(true, ['map', 'set', 'weakSet', 'weakMap', 999, {home: 'lai'}]);
+    console.log(map, map.size, map.get(true), map.get(o));
+    console.log(map.has(true), map.has(o));
+    for (let mapItem of map) {
+        console.log("let of ---", mapItem);
+    }
+    for (let key of map.keys()) {
+        console.log("keys ---", key);
+    }
+    for (let value of map.values()) {
+        console.log("values ---", value);
+    }
+    for (let [key, value] of map.entries()) {
+        console.log("entries ---", key, value);
+    }
+    map.forEach(mapItem => console.log("foreach ---", mapItem));
+    // map.delete(o);
+    map.clear();
+    console.log(map);
+    let mapAno = new Map([[{_id: 37068319920701}, 'ManagerYin'], [true, 'true love'], ['world', 666]]);
+    console.log(mapAno, mapAno.size, mapAno.get(true));
+}
+
+{
+    //WeakMap
+    //1. no size property
+    //2. no clear function
+    //3. no 遍历
+    let weakMap = new WeakMap();
+    let o = {
+        o: '1993-07-01'
+    };
+    weakMap.set(o, {name: 'GaryYin'});
+    console.log(weakMap, weakMap.size, weakMap.get(o));
+    // for (let weakMapItem of weakMap) {
+    //     console.log(weakMapItem);
+    // }
+    // weakMap.clear();
+    // console.log(weakMap);
+    console.log(weakMap.has(o));
+    weakMap.delete(o);
+    console.log(weakMap);
+}
+
+{
+    //Set和array比较
+    let set = new Set();
+    let array = [];
+    //增
+    set.add({t: 66});
+    array.push({t: 66});
+    console.log("set-array-add", set, array);
+    //查
+    console.log(set.has({t: 66}), array.find(arrayItem => arrayItem.t));
 }
